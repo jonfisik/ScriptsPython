8e9a8101273ec7d6520093167a43fee4f1f7acfd
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-04-17 11:27:02 -0300

    Converter
    Converter string para int.

--- Scripts1/ExerciciosPython/ex002.py
+++ Scripts1/ExerciciosPython/ex002.py
@@ -1,3 +1,5 @@
+print('-------------------------------------------------------------')
 nome=input(print('Digite seu nome: '))
 print('Olá,',nome,'seja bem vindo ;)')
 print('É um prazer te conhecer, {}!'.format(nome))
+print('-------------------------------------------------------------')
