2bfe1ea953a193b294483c484f53f646a19a0bd9
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-04-17 11:57:17 -0300

    Create Script04.py

---
+++ Scripts1/Script04.py
@@ -0,0 +1,7 @@
+print('-----------------------------------------------')
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+soma = num1 + num2
+print('A soma de', num1,'com',num2,'é {}!'.format(soma))
+print('A soma de {} com {} e {}!'.format(num1, num2, soma))
+print('-----------------------------------------------')
