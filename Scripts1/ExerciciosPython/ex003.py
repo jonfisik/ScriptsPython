15537f97dfc85e7f3a2282141e56decea5da8c19
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-04-20 16:31:10 -0300

    Create ex003.py

---
+++ Scripts1/ExerciciosPython/ex003.py
@@ -0,0 +1,8 @@
+'''Solicite e considere a entrada de dois números, faça a soma e mostre o resultado'''
+print('--------------------------------------')
+print('Digite dois números inteiros, x e y.')
+x = int(input('Digite o valor de x: ' ))
+y = int(input('Digite o valor de y: ' ))
+soma = x + y
+print('A soma de {} e {} é {}.'.format(x,y,soma))
+print('--------------------------------------')
