b2548bb5c38975ef9bb65fc395beca4d851568ba
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-04-22 20:19:58 -0300

    Create Script07.py

---
+++ Scripts1/Script07.py
@@ -0,0 +1,7 @@
+'''Jonatan Paschoal 22/04/2020 Desafio 7- Crie um programa que receba duas notas, calcule a média e dê o resultado.'''
+print('='*27)
+a = float(input('Digite sua primeira nota: '))
+b = float(input('Digite sua segunda nota: '))
+media = (a + b) / 2
+print('Sua média é {:.2f}.'.format(media))
+print('='*27)
