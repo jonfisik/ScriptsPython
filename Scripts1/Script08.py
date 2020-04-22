231812ed2a22f0bef1a7dc79d88a5f9ac2d50da0
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-04-22 20:32:23 -0300

    Create Script08.py

---
+++ Scripts1/Script08.py
@@ -0,0 +1,7 @@
+'''Jonatan Paschoal 22/04/2020 Desafio 9 - Faça um programa que receba um valor em metros e transforme para centímetros e milímetros.'''
+print('=='*30)
+x = float(input('Digite um valor em metros: '))
+cent = x / 100
+mili = x / 1000
+print('{:.5f} em centímetros é {:.5f} e em milímetros {:.5f}.'.format(x, cent, mili))
+print('=='*30)
