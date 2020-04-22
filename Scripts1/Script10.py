dc11bfcd67fa052f6134567a64929515404dc453
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-04-22 20:54:15 -0300

    Update Script10.py

--- Scripts1/Script10.py
+++ Scripts1/Script10.py
@@ -1,1 +1,5 @@
-'''Jonatan Paschoal 22/04/2020 Desafio 11 - Para a cotação do dólar a 3.27 reais.'''
+'''Jonatan Paschoal 22/04/2020 Desafio 10 - Crie um programa para a cotação do dólar a 3.27 reais e diga quantos dólares ela pode comprar'''
+print('===='*15)
+reais = float(input('Informe quanto reais você tem: '))
+print('Com {:.2f} reais você pode comprar {:.2f} dólares.'.format(reais, reais/3.27))
+print('===='*15)
