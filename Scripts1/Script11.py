5d47cd6c448675d4e112b3cfb090fb269e72a90b
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-04-23 09:36:14 -0300

    Create Script11.py

---
+++ Scripts1/Script11.py
@@ -0,0 +1,9 @@
+'''Escreva um programa que leia a altura e largura de uma parede em metros, calcule sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta é suficiente para 2 metros quadrados.'''
+print('=+='*16)
+Largura = float(input('Qual a medida da largura em metros da parede? '))
+Altura = float(input('Qual a medida da altura em metros da parede? '))
+Area = Largura * Altura
+litro = Area / 2
+print('A área da parede é {:.2f} metros quadrados.'.format(Area))
+print('Quantidade de litros {:.2f}.'.format(litro))
+print('=+='*16)
