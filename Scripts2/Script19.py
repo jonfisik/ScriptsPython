ff0791b6a171f9df20fb8b93070c0f18d896e07c
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-05-06 03:28:27 -0300

    Create Script19.py

---
+++ Scripts2/Script19.py
@@ -0,0 +1,10 @@
+'''Um professor quer sortear um dos seus quatros alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e sorteando o nome do escolhido.'''
+print('------'*5)
+print('Digite o nome de quatro alunos para o sorteio: ')
+a = input('Aluno 1: ')
+b = input('Aluno 2: ')
+c = input('Aluno 3: ')
+d = input('Aluno 4: ')
+
+print('O aluno sorteado é {}!'.format())
+print('------'*5)
