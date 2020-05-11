e7223f0ef99c8fde1c6f3b709da3cf76f00cfa66
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-05-11 08:23:17 -0300

    Create Script24.py

---
+++ Scripts3/Script24.py
@@ -0,0 +1,14 @@
+''' Jonatan Paschoal 11/05/2020 - Crie um programa que receba nome de cidades e verifique se começa com a palavra Santo'''
+import math
+
+print('---------------'*3)
+cidade = input('Digite o nome de uma cidade: ')
+#------------------------------------------------------------
+cidade.upper()
+x = cidade.split()
+#-------------------------------------------------------------
+a = (x[0])
+#-------------------------------------------------------------
+print('O nome da cidade começa com - {}.'.format(a.capitalize()))
+
+print('---------------'*3)
