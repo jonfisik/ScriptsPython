65bd86291dd554f91787bbdba8e597adfe3e4cc7
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-05-14 08:07:33 -0300

    Update Script24.py

--- Scripts3/Script24.py
+++ Scripts3/Script24.py
@@ -10,3 +10,9 @@ a = (x[0])
 print('O nome da cidade começa com - {}.'.format(a.capitalize()))
 
 print('---------------'*3)
+# Resposta alternativa
+print('Resposta alternativa')
+cid = str(input('Em que cidade você nasceu? ')).strip()
+VerdFalso = cid[:5].upper() == 'SANTO'
+print('Verdadeiro ou falso: {}'.format(VerdFalso))
+print('---------------'*3)
