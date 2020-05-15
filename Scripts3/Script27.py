2a8eabd9d79756be6b2ffa8802c7b4e82afead93
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-05-15 18:24:34 -0300

    Update Script27.py

--- Scripts3/Script27.py
+++ Scripts3/Script27.py
@@ -1,2 +1,9 @@
 '''Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e último nome da pessoa.'''
-
+print('--------'*5)
+n = str(input("Digite seu nome: ")).strip()
+nome = n.split()
+print(len(nome))
+print('Prazer em te conhecer.')
+print('Seu primeiro nome é - {}.'.format(nome[0]))
+print('Seu último nome é - {}.'.format(nome[len(nome) - 1]))
+print('--------'*5)
