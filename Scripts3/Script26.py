072d7e9b00c6a1d67cb391090db4f0679efc091a
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-05-15 18:05:07 -0300

    Update Script26.py

--- Scripts3/Script26.py
+++ Scripts3/Script26.py
@@ -1,7 +1,7 @@
 ''' Jonatan Paschoal 11/05/2020 - Crie um programa que receba uma frase e conte quantas vezes a aparecem a letra 'a', qual a primeira posição e última posição que ela aparece.'''
 
 print('---------------'*3)
-frase = input('Digite uma frase: ')
+frase = input('Digite uma frase: ').strip()
 #------------------------------------------------------------
 x = frase.lower() 
 y = x.count('a')
