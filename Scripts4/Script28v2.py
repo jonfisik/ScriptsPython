fad63984b246a6520d11b9b576f0ae8b450b9607
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-05-17 00:02:50 -0300

    Create Script28v2.py

---
+++ Scripts4/Script28v2.py
@@ -0,0 +1,26 @@
+'''Jonatan Paschoal  - 15 /05 /2020. Condicionais if, else. Escreva um programa que faça o computador pensar em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O computador deverá escrever na tela se o usuário venceu ou perdeu.'''
+from random import randint
+from time import sleep
+print('-=-'*15)
+nome = str(input('Olá, qual seu nome? ')).strip()
+print('\n')
+print('Eu sou o computador.')
+print('{}, eu pensei em um número...'. format(nome))
+print('Entre 0 e 5.')
+x = randint(0,5)
+print('\n')
+print('Tente advinhar que número pensei. hahahaha...')
+print('-=-'*15)
+num = int(input('Que número pensei: '))
+print('SUSPENSE...')
+print('Não tenha medo {} hahaha.'.format(nome))
+#--------------------------------------------
+sleep(3)
+if num == x:
+    print('Parabéns você venceu!!!')
+else:
+    print('Você perdeu hahahaha...')
+    print('Pensei no múmero {}.'.format(x))
+    print('Mais sorte da próxima vez.')
+print('Não demore para voltar!')
+print('-=-'*15)
