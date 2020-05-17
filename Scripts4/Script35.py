7674eefe603b13253df4ba605eb01bb2dd7bff72
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-05-16 23:39:44 -0300

    Create Script35.py

---
+++ Scripts4/Script35.py
@@ -0,0 +1,9 @@
+'''Dsenvolva um programa que leia 3 segmentos de reta e verifique se pode formar um triângulo.'''
+print('---------------'*3)
+print('Teste do triangulo.')
+print('Digite 3 medias dos lados de um triângulo.')
+lado1 = float(input('Lado 1: '))
+lado2 = float(input('Lado 2: '))
+lado3 = float(input('Lado 3: '))
+
+print('---------------'*3)
