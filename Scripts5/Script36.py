516511cb168077c7969400253fef697af2d32db8
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-05-21 15:08:34 -0300

    Create Script36.py

---
+++ Scripts5/Script36.py
@@ -0,0 +1,17 @@
+'''Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar. Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.'''
+print('=+=+=+=+=+=+=+=+='*3)
+nome = str(input('Digite seu nome: ')).strip()
+sal = float(input('Informe sua renda mensal: '))
+preco = float(input('Qual o valor da casa? '))
+tempo = int(input('Em quanto anos você deseja parcelar sua conta? '))
+print('-----------------'*3)
+meses = tempo * 12
+parcela = preco/(meses)
+
+if parcela >= (sal * 0.3) and tempo <=15:
+    print('{}, não será possível a compra da casa.'.format(nome))
+    print('A parcela de {:.2f} execede 30% dos seus rendimento.'.format(parcela))
+else:
+    print('Parabéns, você foi aprovado.')
+    print('Sua parcela é de R$ {:.2f}. Distribuidos em {} meses.'.format(parcela,meses))
+print('=+=+=+=+=+=+=+=+='*3)
