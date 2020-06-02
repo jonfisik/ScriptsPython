af7dc12364e15d48f57961fd4e9bc81c85319c20
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-06-02 17:15:03 -0300

    Create Script43.py

---
+++ Script43.py
@@ -0,0 +1,2 @@
+'''Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule se IMC e mostre seu status, de acordo com a tabela abaixo:
+- Abaixo de 18.5: abaixo do peso - Entre 18.5 e 25: peso ideal - 25 até 30: sobrepeso - 30 até 40: obesidade - Acima de 40: Obesidade mórbida.'''
