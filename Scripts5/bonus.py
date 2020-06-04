657cb427bfd227e146e1a3d4e904a41305da11c6
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-06-04 16:29:10 -0300

    Update bonus.py

--- Scripts5/bonus.py
+++ Scripts5/bonus.py
@@ -1,7 +1,9 @@
 from time import sleep
+from datetime import date
 print('=+=+=+=+=+=+=+=+='*3)
 print('Olá, vamos conversar?')
-nome = str(input('Digite seu nome: ')).strip()
+sleep(2)
+nome = str(input('Diga seu nome: ')).strip()
 nome = nome.lower()
 
 if 'jonatan' in nome:
@@ -11,19 +13,27 @@ else:
     print('Uhm, {} que nome mais comum.'.format(nome.capitalize()))
 #-----------------------------------------------------
 if nome == 'jonatan':
-    idade = int(input('Bonitão, qual tua idade? '))
+    idade = int(input('Bonitão, qual tua idade ess ano? '))
     print('{}? Só isso? Sempre Jovem!'.format(idade))
 else:
     print('Pesando...')
-    print('E tua idade?')
-    sleep(3)
+    print('E quantos anos você fez ou faz esse ano? ')
+    sleep(6)
     print('Tá com pressa?')
-    sleep(4)
     idade = int(input('Fala a verdade! '))
     if idade <= 25:
         print('Criança.')
     else:
         print('Tá maduro(a) heim!!')
+anoAtual = date.today().year
+ano = anoAtual - idade
+#----------------------------------------------------
+if ano > 2000:
+    print('{} é do novo século, menos mal!'.format(ano))
+elif ano < 2000 and ano != 1981:
+    print('{} velha guarda. Século passado.'.format(ano))
+elif ano == 1981:
+    print('{}! A safra foi boa!!'.format(ano))
 print('A gente conversa mais depois.')
          
 print('fim')
