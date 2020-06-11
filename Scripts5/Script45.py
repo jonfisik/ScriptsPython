fb81669728d8bbeb68572da1e3ee8736a10c955e
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-06-11 18:14:52 -0300

    Update Script45.py

--- Scripts5/Script45.py
+++ Scripts5/Script45.py
@@ -7,6 +7,14 @@ print('''Sua opcões:
 [1] PAPEL
 [2] TESOURA''')
 jogada = int(input('Qual é a sua jogada: '))
+
+if jogada == 0:
+    jogador = str('PEDRA')
+elif jogada == 1:
+    jogador = str('PAPEL')
+elif jogada == 2:
+    jogador = str('TESOURA')
+#-----------------------------------------------    
 x = random.choice([0,1,2])
 print('=+=+=+=+=+=+=+=+='*3)
 if x == 0:
@@ -16,7 +24,20 @@ elif x == 1:
 elif x == 2:
     pc = str('TESOURA')
 #------------------------------------------------
-
+if jogada == x:
+    resultado = str('EMPATE')
+elif jogada == 0 and x == 1:
+    resultado = str('PC VENCEU!')
+elif jogada == 0 and x == 2:
+    resultado = str('Jogador VENCEU!')
+elif jogada == 1 and x == 0:
+    resultado = str('Jogador VENCEU')
+elif jogada == 1 and x == 2:
+    resultado = str('PC VENCEU')
+elif jogada == 2 and x == 0:
+    resultado = str('PC VENCEU')
+elif jogada == 2 and x == 1:
+    resultado = str('Jogador VENCEU')
 #------------------------------------------------
 sleep(1)
 print('( ( JO ) )')
@@ -26,7 +47,6 @@ sleep(1)
 print('( ( PO ) )')
 print('=+=+=+=+=+=+=+=+='*3)
 print('O computador escolheu {}'.format(pc))
-
+print('O jogador escolheu {}'.format(jogador))
+print('Resultado: {}.'.format(resultado))
 print('=+=+=+=+=+=+=+=+='*3)
-#if jogada == x:
-
