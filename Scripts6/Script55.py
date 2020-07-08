2dfdb2e5d21fb4898b1be4833c24b04d252c0ecc
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-07-08 17:32:49 -0300

    Update Script55.py

--- Scripts6/Script55.py
+++ Scripts6/Script55.py
@@ -3,6 +3,20 @@ print('-=-=-=-=-=-'*4)
 titulo = str('MAIOR E MENOR')
 print('{:^44}'.format(titulo))
 print('-=-=-=-=-=-'*4)
+#---------------------------------------------------
+menor = 0
+maior = 0
 for c in range(1,6):
     peso = float(input('Digite o peso da {}ª pessoa: '.format(c)))
+    if c == 1:
+        peso = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('-----------'*4)
+print('O maior peso lido foi de {} kg.'.format(maior))
+print('O menor poeso lido foi de {} kg.'.format(menor))
 print('-=-=-=-=-=-'*4)
