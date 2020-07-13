25017654257b7f0908b33e8ada42a6384f92f8ab
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-07-12 21:13:30 -0300

    Update Script57.py

--- Scripts7/Script57.py
+++ Scripts7/Script57.py
@@ -3,4 +3,4 @@ sexo = ''
 sexo = str(input('Informe seu sexo [M/F]: ')).strip().upper()[0]
 while sexo not in 'MmFf':
     sexo = str(input('Dados inválidos. Por favor informe seu sexo [M/F]: ')).strip().upper()
-print('Sexo {} registrado com sucesso.'.format(sexo))
+print('Sexo "{}" registrado com sucesso.'.format(sexo))
