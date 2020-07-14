65390265f0b79a26dcacc37e5655aa106adca2d3
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-07-14 20:48:59 -0300

    Create Script59.py

---
+++ Scripts7/Script59.py
@@ -0,0 +1,18 @@
+'''Exercício Python 059: Crie um programa que leia dois valores e mostre um menu na tela:
+[ 1 ] somar
+[ 2 ] multiplicar
+[ 3 ] maior
+[ 4 ] novos números
+[ 5 ] sair do programa
+Seu programa deverá realizar a operação solicitada em cada caso.'''
+print('-----'*6)
+val1 = int(input('Digite o primeiro valor: '))
+val2 = int(input('Digite o segundo valor: '))
+print('-----'*6)
+print('''
+[ 1 ] somar
+[ 2 ] multiplicar
+[ 3 ] maior
+[ 4 ] novos números
+[ 5 ] sair do programa''')
+print('-----'*6)
