e4ffb52136c47416527b5cec44f194134adec1ea
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-07-16 20:57:33 -0300

    Update Script60.py

--- Scripts7/Script60.py
+++ Scripts7/Script60.py
@@ -1,12 +1,18 @@
 '''Exercício Python 060: Faça um programa que leia um número qualquer e mostre o seu fatorial. Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120'''
-print('-----------'*3)
+print('-----------'*4)
 titulo = 'FATORIAL'
-print('{:^30}'.format(titulo))
-print('-----------'*3)
+print('{:^40}'.format(titulo))
+print('-----------'*4)
 num = int(input('''Digite um número
 para calcular seu fatorial: '''))
-x = 1
-while x < num:
-     for x in range(1, num):
-         num = x * num
-         print(num) 
+print('-----------'*4)
+cont = num
+fat = 1
+print('Calculando {}! = '.format(num), end='')
+while cont > 0:
+    print('{}'.format(cont), end='')
+    print(' x ' if cont > 1 else ' = ', end='')
+    fat *= cont # f = f*cont
+    cont -= 1 # cont = cont - 1
+print('{}'.format(fat))
+print('-----------'*4)
