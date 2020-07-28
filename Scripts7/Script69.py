37f0bdebe9a57792e20445dba2f3a4a79c265c79
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-07-27 22:44:14 -0300

    Update Script69.py

--- Scripts7/Script69.py
+++ Scripts7/Script69.py
@@ -1,15 +1,15 @@
 '''Exercício Python 069: Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre:'''
-print('---'*10)
+print('----'*8)
 titulo = 'CADASTRO PESSOA'
 print(f'{titulo:^30}')
 contadorFem = contadorMasc = contadorMaior = contadorMaiorVinte = 0
 opcao = ' '
 while opcao not in 'N':
-    print('---'*10)
+    print('----'*8)
     idade = int(input('Idade: '))
-    sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
-    print('---'*10)
-    opcao = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    sexo = str(input('Sexo: [M/F]: ')).strip().upper()[0]
+    print('----'*8)
+    opcao = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
     #if opcao not in 'FM':
         #print('Digite CORRETAMENTE, por favor.')
     if sexo == 'M':
@@ -20,8 +20,10 @@ while opcao not in 'N':
             contadorMaiorVinte += 1
     if idade >= 18:
         contadorMaior += 1
+print('----'*8)
 print('A todo temos:  ')
 print(f'- {contadorMaior} maior(es) de 18 anos.')
-print(f'- {contadorMasc} homens cadastrado.')
-print(f'- {contadorMaiorVinte} mulheres maiores de 20 anos.')
+print(f'- {contadorMasc} homem(ns) cadastrado.')
+print(f'- {contadorMaiorVinte} mulher(es) maiores de 20 anos.')
+print('----'*8)
 #print(contadorFem,contadorMasc,contadorMaior)
