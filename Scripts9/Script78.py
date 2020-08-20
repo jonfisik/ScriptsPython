ccc48caaa8385dbce0f3a96889fbd9011ea47ee2
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-08-20 11:10:48 -0300

    Update Script78.py

--- Scripts9/Script78.py
+++ Scripts9/Script78.py
@@ -3,7 +3,7 @@ print('=-'*30)
 listanum = []
 maior = menor = 0
 for i in range(0,5):
-    listanum.append(int(input(f'Digite um valor: pa a posição {i}: ')))
+    listanum.append(int(input(f'Digite um valor: para posição {i}: ')))
     if i == 0:
         maior = menor = listanum[i]
     else:
