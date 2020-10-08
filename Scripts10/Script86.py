b7c7a235d13f1080def51c6d8dcdf9fb4f497575
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-10-08 09:18:20 -0300

    Update Script86.py

--- Scripts10/Script86.py
+++ Scripts10/Script86.py
@@ -4,7 +4,7 @@ matriz = [[0,0,0],[0,0,0],[0,0,0]]
 print('-+-'*12)
 for linha in range(0,3):
     for coluna in range(0,3):
-        matriz[linha][coluna] = int(input(f'Digite um valor pata ({linha},{coluna}): '))
+        matriz[linha][coluna] = int(input(f'Digite um valor para ({linha},{coluna}): '))
 print('-+-'*12)
 for linha in range(0,3):
     for coluna in range(0,3):
