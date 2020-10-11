cc8073d3aa3fd26b3839a254618188e5296ef274
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-10-11 11:37:30 -0300

    Create Script90.py

---
+++ Scripts11/Script90.py
@@ -0,0 +1,10 @@
+'''Exercício Python 090: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.'''
+dados = dict()
+condicao = list()
+dados['nome'] = str(input('Nome: '))
+dados['media'] = float(input(f'Média de "{dados["nome"]}": '))
+condicao.append(dados.copy())
+print('=+='*10)
+if dados['media'] >= 7:
+        print('aprovado')
+print(condicao)
