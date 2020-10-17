0ec92585e93ad9ae3e85bc75b46bb4874c00c7c5
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-10-17 18:56:14 -0300

    Create Script96.py

---
+++ Sripts12/Script96.py
@@ -0,0 +1,8 @@
+'''Exercício Python 096: Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.'''
+#def area(larg, comp):
+
+
+
+
+print(f"{'Cálculo de área':^20}")
+print("--"*10)
