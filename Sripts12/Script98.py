941a9a0aa9b66a0ab5df7b19f260bd6992b9afe2
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-10-18 10:09:22 -0300

    Update Script98.py

--- Sripts12/Script98.py
+++ Sripts12/Script98.py
@@ -2,3 +2,42 @@
 a) de 1 até 10, de 1 em 1
 b) de 10 até 0, de 2 em 2
 c) uma contagem personalizada'''
+from time import sleep
+
+def contador(i, f, p):
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+
+    print('-=-'*20)
+    print(f'Contagem de {i} até {f} de {p} em {p}.')
+    sleep(2)
+
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.3)
+            cont += p
+        print("FIM!")
+        print()
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='')
+            sleep(0.3)
+            cont -= p
+        print('FIM!')
+        print()
+    
+# Programa principal
+contador(1, 10, 1)
+contador(10, 1, 2)
+print('-=-'*20)
+print('Agora é sua vez de persolnalizar a contagem.')
+ini = int(input('Início: '))
+fim = int(input('Fim: '))
+passo = int(input('Passo: '))
+contador(ini, fim, passo)
+print('-=-'*20)
