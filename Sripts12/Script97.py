7db9231ff2f2a28fb7b8a5dcae3c60163d23f4d4
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-10-18 09:31:22 -0300

    Create Script97.py

---
+++ Sripts12/Script97.py
@@ -0,0 +1,11 @@
+'''Exercício Python 097: Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.'''
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~'*tam)
+    print(f'  {msg}')
+    print('~'*tam)
+
+# programa principal
+x = str(input('Digite uma palavra ou frase: '))
+#escreva = x
+escreva(x)
