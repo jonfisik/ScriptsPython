d2b5aa7569da2d5e1c667b9b44a749ee4ccde5f1
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-10-17 21:12:18 -0300

    Update Script96.py

--- Sripts12/Script96.py
+++ Sripts12/Script96.py
@@ -1,8 +1,16 @@
 '''Exercício Python 096: Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.'''
-#def area(larg, comp):
+# Função
+def area(larg, comp):
+    a = larg * comp
+    print(f'A área de um terreno {larg} m x {comp} m é de {a} m²') # tabela ASCII: alt + 253
+    print("--"*25)
 
+# Programa principal
+print("--"*25)
+print(f"{'Cálculo de área':^50}")
+print("--"*25)
 
+l = float(input('Largura (m): '))
+c = float(input('Comprimento (m): '))
 
-
-print(f"{'Cálculo de área':^20}")
-print("--"*10)
+area(l, c)
