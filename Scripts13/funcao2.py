dc0336aaf79da63546ba89f02eab660389ad414f
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-10-19 18:41:09 -0300

    Create funcao2.py

---
+++ Scripts13/funcao2.py
@@ -0,0 +1,21 @@
+# docstring
+
+def contador(i,f,p):
+    """
+    A função faz uma contagem e mostra na tela
+    i: início da contagem
+    f: fim da contagem
+    p: passo da contagem
+    return: sem retorno
+    """
+    c = i
+    while c <= f:
+        print(f'{c} ', end='')
+        c += p
+    print('FIM!')
+
+#contador(10, 100, 2.5)
+
+help(contador)
+
+
