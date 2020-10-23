6034e874d33961f963039f7cb561868220d1bf3e
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-10-22 21:08:22 -0300

    Create Script103.py

---
+++ Scripts13/Script103.py
@@ -0,0 +1,16 @@
+'''Exercício Python 103: Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.'''
+def ficha(jog='<desconhecido>', gol=0):
+    print(f'O (a) {jog} fez {gol} gol(s) no campeonato.')
+
+#programa 
+print('---'*15)
+n = str(input('Nome do jogador: '))
+g = str(input('Número de gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gol=g)
+else:
+    ficha(n, g)
