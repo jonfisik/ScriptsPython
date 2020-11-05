df5f9aa1fdd41f674200610c9119f09d77e087e6
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-11-05 02:44:46 -0300

    Update sistema.py

--- Scripts15/Script115/sistema.py
+++ Scripts15/Script115/sistema.py
@@ -10,12 +10,20 @@ if not arquivoExiste(arq):
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        cabecalho('Opção 1')
+        #Opção de listar o conteúdo de um arquivo. 
+        #cabecalho('PESSOAS CADASTRADAS')
+        lerArquivo(arq)
     elif resposta == 2:
-        cabecalho('Opção 2')
+        #Opção de cadastrar uma nova pessoa.
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
+        #Opção de sair do sistema.
         cabecalho('Saindo do sistema... Até logo!')
         break
     else:
+        #Digitou uma opção errada no menu.
         print('\033[31mERRO! Digite uma opção válida!\033[m')
     sleep(2)
